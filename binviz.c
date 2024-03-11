7c96c64bea93344c469d4c97ddae8978b68185f6
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2024-03-11 15:02:36 +0000

    Replace sprintf() with snprintf() to avoid a buffer overflow if the file path is longer than 256. Remove map and reuse pixels for both processing the file onto the map and then rendering it onto pixels. Move the rendering to another function. Previously the program was overwriting pixels each time it was processing a chunk. Save the last byte to of each chunk and increment it with the first byte of the next chunk. Previously, all the last bytes of chunks were being ignored. Add checks for division by zero and pole error in log.

--- binviz.c
+++ binviz.c
@@ -5,6 +5,15 @@
 #include <errno.h>
 #include <tgmath.h>
 
+/* C2X/C23 or later? */
+#if defined(__STDC__VERSION) && __STDC_VERSION__ >= 202000L
+    #include <stddef.h>     /* nullptr_t */
+#else
+    #include <stdbool.h>    /* bool, true. false */
+    #define nullptr ((void *)0)
+    typedef void * nullptr_t;
+#endif                      /* nullptr/nullptr_t */
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_STATIC
 #include "stb_image_write.h"
@@ -14,7 +23,6 @@
 #define OUTPUT_FPATH_SIZE   256
 #define CHUNK_SIZE          1024 * 8
 
-static size_t map[MAP_SIZE][MAP_SIZE];
 static int32_t pixels[MAP_SIZE][MAP_SIZE];
 
 static FILE *xfopen(const char path[static 1])
@@ -24,13 +32,15 @@ static FILE *xfopen(const char path[static 1])
 
     if (!fp) {
         fprintf(stderr, "Error: could not open file %s: %s.\n", path,
-            errno ? strerror(errno) : "");
+            errno ? strerror(errno) : "unknown error");
         exit(EXIT_FAILURE);
     }
     return fp;
 }
 
-static char *read_next_chunk(FILE *stream, char *chunk, size_t *size)
+static bool read_next_chunk(FILE stream[restrict static 1], 
+                            char chunk[restrict static CHUNK_SIZE], 
+                            size_t *size)
 {
     if (size) {
         *size = 0;
@@ -40,12 +50,12 @@ static char *read_next_chunk(FILE *stream, char *chunk, size_t *size)
 
     if (rcount < CHUNK_SIZE) {
         if (!feof(stream)) {
-            /* A read error occured. */
-            return NULL;
+            /* A read error occurred. */
+            return false;
         }
 
         if (rcount == 0) {
-            return NULL;
+            return false;
         }
     }
     
@@ -55,47 +65,62 @@ static char *read_next_chunk(FILE *stream, char *chunk, size_t *size)
         *size = rcount;
     }
 
-    return chunk;
+    return true;;
 }
 
-static _Bool process_file(FILE stream[static 1])
+static void render_pixels(size_t max_val)
+{
+    float max = 0;
+    
+    if (max_val) {
+        max = log(max_val);
+    } else {
+        return;
+    }
+    
+    for (size_t y = 0; y < MAP_SIZE; ++y) {
+        for (size_t x = 0; x < MAP_SIZE; ++x) {
+            const float t = log(pixels[y][x]) / max;
+            const uint32_t b = t * MAP_SIZE;
+
+            pixels[y][x] = 0xff000000 | b | (b << 8u) | (b << 16u);
+        }
+    }
+}
+
+static bool process_file(FILE stream[static 1], int32_t *max_val)
 {
     char content[CHUNK_SIZE];
-    char *p = NULL;
     size_t nbytes = 0;
+    bool is_first = true;
+    uint8_t last_byte = 0;
+    *max_val = 0;
 
-    while ((p = read_next_chunk(stream, content, &nbytes))) {
+    while (true) {
+        bool rv = read_next_chunk(stream, content, &nbytes);
+
+        if (!rv) {
+            break;
+        }
+        
+        if (!is_first) {
+            pixels[last_byte][(uint8_t)content[0]] += 1;
+        } else {
+            is_first = false;
+        }
+
         for (size_t i = 0; i < nbytes - 1; ++i) {
-            uint8_t x = (uint8_t)content[i];
-            uint8_t y = (uint8_t)content[i + 1];
+            const uint8_t x = (uint8_t)content[i];
+            const uint8_t y = (uint8_t)content[i + 1];
 
-            map[y][x] += 1;
-        }
+            pixels[y][x] += 1;
 
-        float max = 0;
-
-        for (size_t y = 0; y < MAP_SIZE; ++y) {
-            for (size_t x = 0; x < MAP_SIZE; ++x) {
-                float f = 0.0f;
-
-                if (map[y][x] > 0) {
-                    f = log(map[y][x]);
-                }
-
-                if (f > max) {
-                    max = f;
-                }
+            if (pixels[y][x] > *max_val) {
+                *max_val = pixels[y][x];
             }
         }
 
-        for (size_t y = 0; y < MAP_SIZE; ++y) {
-            for (size_t x = 0; x < MAP_SIZE; ++x) {
-                float t = log(map[y][x]) / max;
-                uint32_t b = t * MAP_SIZE;
-
-                pixels[y][x] = 0XFF000000 | b | (b << 8) | (b << 16);
-            }
-        }
+        last_byte = (uint8_t)content[nbytes - 1];
     }
 
     return feof(stream);
@@ -120,24 +145,26 @@ int main(int argc, char **argv)
 
     const char *const input = argv[1];
     FILE *const fp = xfopen(input);
-    
-    if (!process_file(fp)) {
-        fprintf(stderr, "Error: an unexpected error occured while reading.\n");
+    int32_t max_val = 0;
+
+    if (!process_file(fp, &max_val)) {
+        fprintf(stderr, "Error: an unexpected error occurred while reading.\n");
         fclose(fp);
         return EXIT_FAILURE;
     }
-
+    
     fclose(fp);
+    render_pixels(max_val);
 
     char out_fpath[OUTPUT_FPATH_SIZE];
 
-    sprintf(out_fpath, "%s" OUTPUT_FNAME_EXT, input);
+    snprintf(out_fpath, OUTPUT_FPATH_SIZE, "%s" OUTPUT_FNAME_EXT, input);
     const int rv =
-        stbi_write_png(out_fpath, MAP_SIZE, MAP_SIZE, 4, pixels,
+        stbi_write_png(out_fpath, MAP_SIZE, MAP_SIZE, sizeof pixels[0][0], pixels,
         MAP_SIZE * sizeof pixels[0][0]);
 
     if (rv == 0) {
-        fprintf(stderr, "Error: could not save image %s.\n", out_fpath);
+        fprintf(stderr, "Error: could not generate image %s.\n", out_fpath);
         return EXIT_FAILURE;
     }
 
